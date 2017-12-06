3b8e30035cf5e5f005a71eaab7c689e246487650
zdimon <zdimon77@gmail.com>
2017-12-06 18:35:36 +0300

    auto commit

--- want2buy/_local.py
+++ want2buy/_local.py
@@ -1,4 +1,4 @@
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -8,12 +8,11 @@ DATABASES = {
         'HOST': 'localhost',
     }
 }
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
