fbaf8001d77f74600878a4d02b880cf921c9a0c9
zdimon <zdimon77@gmail.com>
2017-12-08 18:52:53 +0300

    fix created_at in replanishments

--- replenishment/models.py
+++ replenishment/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from account.models import Profile
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
 
@@ -19,6 +20,6 @@ class Replanishment(models.Model):
     Replanishment_system = models.CharField(verbose_name='платежная система', max_length=150, choices=Replanishment_system_choices, default=MasterCard)
     user_replanishment = models.ForeignKey(User)
     ammount = models.FloatField(verbose_name='сумма')
-    created_at = models.DateTimeField(default=timezone.now())
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
 
 
