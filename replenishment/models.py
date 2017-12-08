f1c10e827cf529e8d10f6852a4e8d6a21fad548a
mikhail1706 <mgumenm@gmail.com>
2017-12-08 09:49:20 +0200

    replanishment

--- replenishment/models.py
+++ replenishment/models.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 from account.models import Profile
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Replanishment(models.Model):
-    user_replanishment = models.ForeignKey(Profile.user)
+    user_replanishment = models.ForeignKey(User)
     ammount = models.FloatField()
     created_at = models.DateTimeField(default=timezone.now())
     system = models.CharField(max_length=150)
