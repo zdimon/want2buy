0f1532191a9bdc38957da35bf7428173c3166bee
Dj_Denis <dj_denis97@gmail.com>
2017-12-12 13:19:32 +0200

    Announcement api

--- api/views.py
+++ api/views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 from jsonview.decorators import json_view
 from django.core.cache import cache
 from catalog.models import Region, City, Category, SubCategory, SubSubCategory
-
-
+from archive.models import Announcement
+from django.core import serializers
+import json
 @json_view
 def regions(request):
     if not cache.get('regions'):
@@ -37,4 +37,20 @@ def categories(request):
         cache.set('categories', out, 60*60*24)
     else:
         out = cache.get('categories')
+    return out
+
+
+@json_view
+def announcement(request, slug):
+    if len(Announcement.objects.filter(id=slug)) != 0:
+        tmp = json.loads(serializers.serialize('json', Announcement.objects.filter(id=slug)))[0]['fields']
+        out = {'user_id': tmp['user'], 'title': tmp['title'], 'category': Category.objects.get(id=tmp['category']).name,
+               'sub_category': SubCategory.objects.get(id=tmp['sub_category']).name,
+               'sub_sub_category': SubSubCategory.objects.get(id=tmp['sub_sub_category']).name,
+               'new_category': tmp['new_category'], 'new_bu': tmp['new_bu'], 'opt_roznica': tmp['opt_roznica'],
+               'type': tmp['type'], 'once': tmp['once'], 'price': tmp['price'], 'amount': tmp['ammount'],
+               'city': City.objects.get(id=tmp['city']).name, 'photo': Announcement.objects.get(id=slug).photo.url,
+               'created_at': tmp['created_at'], 'is_paid': tmp['is_paid'], 'expire': tmp['date_expire'], 'paid_expire': tmp['date_paid_expire']}
+    else:
+        out = {'message': 'No such element'}
     return out
