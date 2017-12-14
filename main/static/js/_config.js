e0d033e122c5b33ae6b6ea0bc4d2475d6c5f13c9
Nick <hukutocoo747596@gmail.com>
2017-12-14 13:16:04 +0200

    Some view changes for footer navbar.

--- main/static/js/_config.js
+++
@@ -1,3 +0,0 @@
-w2b_app_config = {
-    'host': 'http://localhost:8080'
-}
---
+++ main/static/js/main.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+
+    $('ul.header-menu li').click(function(){
+        window.sessionStorage.currentMenuItem = this.id;
+    });
+
+    $('h1.site-title a').click(function () {
+        sessionStorage.currentMenuItem = "hnHome";
+    });
+
+    switch (sessionStorage.currentMenuItem)
+    {
+        case 'fnHome':
+            sessionStorage.currentMenuItem = 'hnHome';
+            break;
+        case 'fnAbout':
+            sessionStorage.currentMenuItem = 'hnAbout';
+            break;
+        case 'fnRules':
+            sessionStorage.currentMenuItem = 'hnRules';
+            break;
+        case 'fnFeedback':
+            sessionStorage.currentMenuItem = 'hnFeedback';
+            break;
+    }
+
+    $("#"+sessionStorage.currentMenuItem).addClass('current-menu-item menu-item-has-children');
+
+
+})();
