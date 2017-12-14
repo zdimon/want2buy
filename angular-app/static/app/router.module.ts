fe8da0ac32e4677150969e9224ccc62870f682b4
zdimon <zdimon@example.com>
2017-12-14 18:22:35 +0200

    changes during meeting 14-12-2017

--- angular-app/static/app/router.module.ts
+++ angular-app/static/app/router.module.ts
@@ -5,7 +5,7 @@ import { IndexComponent } from './index.component'
 import { ActiveAnnouncementsComponent } from './active.announcements.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: '', redirectTo: 'announcements', pathMatch: 'full' }, 
   { path: 'edit/announcement/:announcement_id', component: EditAnnouncementComponent },
   { path: 'index', component: IndexComponent },
   { path: 'announcements', component: ActiveAnnouncementsComponent }
---
+++ archive/management/commands/load_offer.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from catalog.models import *
+from account.models import Profile
+from want2buy.settings import BASE_DIR
+import json
+from django.core.files import File
+import shutil
+from archive.models import NewAnnouncement, Announcement, Offer, OfferMessage
+from catalog.models import *
+
+ 
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        print 'Clearing the table'
+        Offer.objects.all().delete()
+        print 'Start loading..'
+        for a in Announcement.objects.all().order_by('-id'):
+            print 'Process announcement...%s' % a.id
+            for u in User.objects.exclude(id=a.user_id):
+                print 'Saving .....'
+                o = Offer()
+                o.user = u
+                o.message = 'test message'
+                o.url = 'test url'
+                o.price = 10
+                o.save()
+
+                m = OfferMessage()
+                m.offer = o
+                m.new_price = 20
+                m.message = 'I have a new dial'
+                m.user = u
+                m.save()
+
+                m = OfferMessage()
+                m.offer = o
+                m.new_price = 20
+                m.message = 'I have a new dial 2'
+                m.user = u
+                m.save()
+
+            #print a.user
+            #break
+           
