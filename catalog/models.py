e49f0752372aa757e724cfd15a95f76a70d055a1
Dj_Denis <dj.denis97@gmail.com>
2017-12-17 11:35:54 +0200

    #26 verbose_name

--- catalog/models.py
+++ catalog/models.py
@@ -5,14 +5,17 @@ from django.db import models
 from slugify import slugify
 from image_cropping import ImageRatioField
 from image_cropping.utils import get_backend
+from django.utils.text import ugettext_lazy as _
+
 
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, verbose_name=_(u'Название'))
     name_slug = models.CharField(max_length=150)
-    photo = models.ImageField(upload_to='catalog_pic/', null=True, blank=True)
-    cropping = ImageRatioField('photo', '255x200')    
+    photo = models.ImageField(upload_to='catalog_pic/', null=True, blank=True, verbose_name=_(u'Фото'))
+    cropping = ImageRatioField('photo', '255x200', verbose_name=_(u'Кадрирование'))
+
     def get_absolute_url(self):
         return reverse('catalog_main', kwargs={'slug': self.name_slug})
 
@@ -22,7 +25,6 @@ class Category(models.Model):
     def save(self, *args, **kwargs):
         self.name_slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
-
 
     def thumbnail(self):
         try:
@@ -38,11 +40,13 @@ class Category(models.Model):
             return '<img class="img-responsive" src="%s"  />' % thumbnail_url
         except:
             return '<img class="img-responsive" src="/static/images/noimage.png" />'
+
     thumbnail.allow_tags = True
 
+
 class SubCategory(models.Model):
-    name = models.CharField(max_length=150)
-    parent_category = models.ForeignKey(Category)
+    name = models.CharField(max_length=150, verbose_name=_(u'Название'))
+    parent_category = models.ForeignKey(Category, verbose_name=_(u'Базовая категория'))
     name_slug = models.CharField(max_length=150)
 
     def get_absolute_url(self):
@@ -57,17 +61,17 @@ class SubCategory(models.Model):
 
 
 class SubSubCategory(models.Model):
-    name = models.CharField(max_length=150)
-    parent_sub_category = models.ForeignKey(SubCategory)
+    name = models.CharField(max_length=150, verbose_name=_(u'Название'))
+    parent_sub_category = models.ForeignKey(SubCategory, verbose_name=_(u'Базовая подкатегория'))
     name_slug = models.CharField(max_length=150)
-    
+
     def get_absolute_url(self):
         return reverse('catalog_sub_sub', kwargs={'slug': self.name_slug})
 
     @property
     def parent_category(self):
         return self.parent_sub_category.parent_category
-    
+
     def __unicode__(self):
         return self.name
 
@@ -75,8 +79,9 @@ class SubSubCategory(models.Model):
         self.name_slug = slugify(self.name)
         super(SubSubCategory, self).save(*args, **kwargs)
 
+
 class Region(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, verbose_name=_(u'Название'))
     name_slug = models.CharField(max_length=150)
 
     def __unicode__(self):
@@ -88,8 +93,8 @@ class Region(models.Model):
 
 
 class City(models.Model):
-    name = models.CharField(max_length=150)
-    region = models.ForeignKey(Region)
+    name = models.CharField(max_length=150, verbose_name=_(u'Название'))
+    region = models.ForeignKey(Region, verbose_name=_(u'Регион'))
     lat = models.FloatField()
     lon = models.FloatField()
 
